c954412a50fe494ac7fe50dd2e153a273d29bdc6
mohamed-eg <mohammed.akel@pme.suezuni.edu.eg>
2025-05-09 23:14:43 +0300

    K.O

---
+++ helpers/cloudinary.js
@@ -0,0 +1,22 @@
+const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
+
+cloudinary.config({
+  cloud_name: "dzfftbfde",
+  api_key: "983815788533856",
+  api_secret: "oh9O_OxeZr2hm8soW9m_auj-2dQ",
+});
+
+const storage = new multer.memoryStorage();
+
+async function imageUploadUtil(file) {
+  const result = await cloudinary.uploader.upload(file, {
+    resource_type: "auto",
+  });
+
+  return result;
+}
+
+const upload = multer({ storage });
+
+module.exports = { upload, imageUploadUtil };
