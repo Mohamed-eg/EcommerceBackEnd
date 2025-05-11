1b02e44011f2259749477ead2879cb01ba0e8743
mohamed-eg <mohammed.akel@pme.suezuni.edu.eg>
2025-05-11 21:11:30 +0300

    FixCORE Error

--- server.js
+++ server.js
@@ -25,10 +25,16 @@ mongoose
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const allowedOrigins = ['https://ecommerce-front-end-ashy.vercel.app'];
 app.use(
   cors({
-    origin: "https://ecommerce-front-end-ashy.vercel.app/",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
